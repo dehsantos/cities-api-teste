23eb20b9380e87013e52732ea14316354ac163dd
Andre <Andre.Santos2@globalweb.com.br>
2021-11-06 20:25:07 -0300

    primeiro commit

---
+++ src/test/java/com/andresantos/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresantos.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
